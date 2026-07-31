91dd415629393bd6c57e468184b35d62f0e9919b
Sifan Terefe <sifanfekadu@gmail.com>
2026-07-31 11:06:08 -0400

    Implemented functions freeMatchData and promptUserRestart in Exit.c

--- Project_II_CSCN71030/Exit.c
+++ Project_II_CSCN71030/Exit.c
@@ -12,3 +12,21 @@ void printChampion(const Team* champion) {
         champion->wins, champion->draws, champion->losses,
         champion->goal_for, champion->goal_against);
 }
+
+void freeMatchData(ChampionshipRecord* record) {
+    free(record->path);
+    record->path = NULL;
+    record->pathLength = 0;
+}
+
+int promptUserRestart(void) {
+    int choice;
+    do {
+        printf("Enter 1 to run a new simulation, 0 to exit: ");
+        if (scanf("%d", &choice) != 1) {
+            while (getchar() != '\n');
+            choice = -1;
+        }
+    } while (choice != 0 && choice != 1);
+    return choice;
+}
