c4e992c1723df0685dc796185d3bbf9ed244ffb9
Sifan Terefe <sifanfekadu@gmail.com>
2026-07-31 12:54:25 -0400

    Removed Goals For/Against statistic from the final champion's record to be printed in the printChampion function in the Exit.c source file

--- Project_II_CSCN71030/Exit.c
+++ Project_II_CSCN71030/Exit.c
@@ -7,16 +7,15 @@ void printChampion(const Team* champion) {
         printf("No Champion!\n");
         return;
     }
-    printf("=== CHAMPION: %s (Group %c) ===\n", champion->name, champion->group);
+    printf("=== WORLD CHAMPION: %s (Group %c) ===\n", champion->name, champion->group);
     printf("Record: %dW-%dD-%dL, GF:%d GA:%d GD:%d\n",
-        champion->wins, champion->draws, champion->losses,
-        champion->goal_for, champion->goal_against);
+        champion->wins, champion->draws, champion->losses);
 }
 
 ChampionshipRecord buildChampionshipPath(Match fullHistory[], int numMatches,
     const Team* champion) {
     ChampionshipRecord record = { champion, NULL, 0 };
-    int count = 0;
+    int count = 0; 
 
     for (int i = 0; i < numMatches; i++)
         if (fullHistory[i].team1 == champion || fullHistory[i].team2 == champion)
