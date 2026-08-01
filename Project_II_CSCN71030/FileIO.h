eb1b6472cea5cf0299075a9425abc44abbff348b
MiguelTAR <mtarazona4489@conestogac.on.ca>
2026-07-31 20:21:55 -0400

    FileIO module.

--- Project_II_CSCN71030/FileIO.h
+++ Project_II_CSCN71030/FileIO.h
@@ -1,1 +1,19 @@
-#pragma once
+#ifndef fileio_h
+#define fileio_h
+
+#include "Team.h"
+
+int count_teams(const char* filename);
+
+Team* Load_teams(const char* filename, int* team_count);
+
+void Save_results(const char* filename, const Team* teams, int team_count);
+
+
+void free_teams(Team* teams);
+
+
+
+
+
+#endif 
