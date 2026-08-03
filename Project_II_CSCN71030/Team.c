c78a7208d40838656927478a17a9c622f8bcb38b
MiguelTAR <mtarazona4489@conestogac.on.ca>
2026-08-03 19:05:40 -0400

    Comments for the team.c file

--- Project_II_CSCN71030/Team.c
+++ Project_II_CSCN71030/Team.c
@@ -1,7 +1,11 @@
+// Miguel Tarazona
+// This program defines the Team structure and
+// provides functions to initialize and print team information.
 #include <stdio.h>
 #include "Team.h"
 #include <string.h>
 
+// Function to create and initialize a Team structure.
 void initialize_team(Team* team, const char* name, char group)
 {
 	strcpy(team->name, name);
@@ -9,22 +13,18 @@ void initialize_team(Team* team, const char* name, char group)
 	team->wins = 0;
 	team->losses = 0;
 	team->draws = 0;
-	team->goal_for = 0;
-	team->goal_against = 0;
 }
 
+// Function to print the information of a single team.
 void print_team(const Team* team)
 {
 	printf("%s Group: %c\n", team->name, team->group);
 	printf("Wins: %d\n", team->wins);
 	printf("Losses: %d\n", team->losses);
 	printf("Draws: %d\n", team->draws);
-	printf("Goals For: %d\n", team->goal_for);
-	printf("Goals Against: %d\n", team->goal_against);
-
 }
 
-
+// Function to print the information of all teams in an array.
 void print_all(const Team* teams, int num_teams)
 {
 	for (int i = 0; i < num_teams; i++)
@@ -34,4 +34,10 @@ void print_all(const Team* teams, int num_teams)
 
 }
 
+/* 
+The reason for the team module is to provide a
+clear and organized way to manage and display information about futbol teams. 
+The Team structure holds essential data about each team, including its name,
+group, and performance statistics (wins, losses, draws).
+*/
 
