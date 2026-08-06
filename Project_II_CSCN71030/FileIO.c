59ab11e4f6680e1e804a033cca301d3d4a9fecee
MiguelTAR <mtarazona4489@conestogac.on.ca>
2026-08-06 19:28:22 -0400

    Finishing comments from fielIO

--- Project_II_CSCN71030/FileIO.c
+++ Project_II_CSCN71030/FileIO.c
@@ -1,18 +1,22 @@
+// Miguel Tarazona
+// File Input and Output main c file
 #include "FileIO.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include"Team.h"
 
-
+//count the number of teams in the file.
 int count_teams(const char* filename)
 {
-	FILE* file = fopen(filename, "r");
+	FILE* file = fopen(filename, "r");// Open the file for reading
 	if (file == NULL) {
-		printf("Error opening file %s\n", filename);
-		return -1;	
+		printf("Error opening file %s\n", filename);// Check if the file was opened successfully
+		return -1;// Return -1 to indicate an error
 }
 
+	// while loop to read each line of the file and 
+	// count the number of teams at first starting with 0
 	int count = 0;
 	char line[100];
 	while (fgets(line, sizeof(line), file)) {
@@ -22,6 +26,8 @@ int count_teams(const char* filename)
 	return count;
 }
 
+
+// Load the teams from the file into an array of Team structs
 Team* Load_teams(const char* filename, int* team_count)
 {
 	*team_count = count_teams(filename);
@@ -46,6 +52,7 @@ Team* Load_teams(const char* filename, int* team_count)
 	char line[100];
 	int index = 0;
 
+	// Read each line of the file and parse the team name and group
 	while (fgets(line, sizeof(line), file)) {
 		char group;
 		char team_name[50];
@@ -65,7 +72,7 @@ Team* Load_teams(const char* filename, int* team_count)
 	return teams;
 }
 
-
+//	Save the results of the teams to a file
 void save_results(const char* filename, const Team* teams, int team_count)
 {
 	FILE* file = fopen(filename, "w");
@@ -73,7 +80,7 @@ void save_results(const char* filename, const Team* teams, int team_count)
 		printf("Error opening file %s for writing\n", filename);
 		return;
 	}
-	fprintf(file, "TEAM, MP, W, L, D%s\n");
+	fprintf(file, "TEAM, MP, W, L, D\n");
 	for(int i = 0; i < team_count; i++) {
 		fprintf(file, "%s, %d, %d, %d, %d\n", teams[i].name, teams[i].matches_played, 
 			teams[i].wins, teams[i].losses, teams[i].draws);
@@ -82,7 +89,7 @@ void save_results(const char* filename, const Team* teams, int team_count)
 
 	printf("Results saved %s\n", filename);
 }
-
+// dynamically free the memory allocated for the teams array
 void free_teams(Team* teams)
 {
 	if (teams != NULL) {
@@ -90,3 +97,7 @@ void free_teams(Team* teams)
 		printf("Memory freed for teams\n");
 	}
 }
+
+/*This program is a C implementation of file input and output operations for managing a list of teams.
+* It defines a Team struct to hold information about each team, including its name, group, and match statistics.
+*/
